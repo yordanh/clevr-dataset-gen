e395ae456d9f281f6e220ac81b4f38e0fedd4c0a
Yordan Hristov <yshristov@gmail.com>
2019-06-18 15:35:08 +0100

    change default param values; change output scheme

--- image_generation/render_images.py
+++ image_generation/render_images.py
@@ -10,6 +10,11 @@ import math, sys, random, argparse, json, os, tempfile
 from datetime import datetime as dt
 from collections import Counter
 
+import get_b_box
+
+import numpy as np
+import os.path as osp
+
 """
 Renders random scenes using Blender, each with with a random number of objects;
 each object has a random size, position, color, and shape. Objects will be
@@ -68,17 +73,17 @@ parser.add_argument('--min_objects', default=3, type=int,
     help="The minimum number of objects to place in each scene")
 parser.add_argument('--max_objects', default=10, type=int,
     help="The maximum number of objects to place in each scene")
-parser.add_argument('--min_dist', default=0.25, type=float,
+parser.add_argument('--min_dist', default=0.4, type=float,
     help="The minimum allowed distance between object centers")
 parser.add_argument('--margin', default=0.4, type=float,
     help="Along all cardinal directions (left, right, front, back), all " +
          "objects will be at least this distance apart. This makes resolving " +
          "spatial relationships slightly less ambiguous.")
-parser.add_argument('--min_pixels_per_object', default=200, type=int,
+parser.add_argument('--min_pixels_per_object', default=150, type=int,
     help="All objects will have at least this many visible pixels in the " +
          "final rendered images; this ensures that no objects are fully " +
          "occluded by other objects.")
-parser.add_argument('--max_retries', default=50, type=int,
+parser.add_argument('--max_retries', default=20, type=int,
     help="The number of times to try placing an object before giving up and " +
          "re-placing all objects in the scene.")
 
@@ -95,19 +100,11 @@ parser.add_argument('--split', default='new',
     help="Name of the split for which we are rendering. This will be added to " +
          "the names of rendered images, and will also be stored in the JSON " +
          "scene structure for each image.")
-parser.add_argument('--output_image_dir', default='../output/images/',
-    help="The directory where output images will be stored. It will be " +
+parser.add_argument('--output_dir', default='../output',
+    help="The directory where output data will be stored. It will be " +
          "created if it does not exist.")
-parser.add_argument('--output_scene_dir', default='../output/scenes/',
-    help="The directory where output JSON scene structures will be stored. " +
-         "It will be created if it does not exist.")
-parser.add_argument('--output_scene_file', default='../output/CLEVR_scenes.json',
-    help="Path to write a single JSON file containing all scene information")
-parser.add_argument('--output_blend_dir', default='output/blendfiles',
-    help="The directory where blender scene files will be stored, if the " +
-         "user requested that these files be saved using the " +
-         "--save_blendfiles flag; in this case it will be created if it does " +
-         "not already exist.")
+
+
 parser.add_argument('--save_blendfiles', type=int, default=0,
     help="Setting --save_blendfiles 1 will cause the blender scene file for " +
          "each generated image to be stored in the directory specified by " +
@@ -131,13 +128,13 @@ parser.add_argument('--width', default=320, type=int,
     help="The width (in pixels) for the rendered images")
 parser.add_argument('--height', default=240, type=int,
     help="The height (in pixels) for the rendered images")
-parser.add_argument('--key_light_jitter', default=1.0, type=float,
+parser.add_argument('--key_light_jitter', default=0, type=float,
     help="The magnitude of random jitter to add to the key light position.")
-parser.add_argument('--fill_light_jitter', default=1.0, type=float,
+parser.add_argument('--fill_light_jitter', default=0, type=float,
     help="The magnitude of random jitter to add to the fill light position.")
-parser.add_argument('--back_light_jitter', default=1.0, type=float,
+parser.add_argument('--back_light_jitter', default=0, type=float,
     help="The magnitude of random jitter to add to the back light position.")
-parser.add_argument('--camera_jitter', default=0.5, type=float,
+parser.add_argument('--camera_jitter', default=0, type=float,
     help="The magnitude of random jitter to add to the camera position")
 parser.add_argument('--render_num_samples', default=512, type=int,
     help="The number of samples to use when rendering. Larger values will " +
@@ -158,52 +155,55 @@ def main(args):
   img_template = '%s%%0%dd.png' % (prefix, num_digits)
   scene_template = '%s%%0%dd.json' % (prefix, num_digits)
   blend_template = '%s%%0%dd.blend' % (prefix, num_digits)
-  img_template = os.path.join(args.output_image_dir, img_template)
-  scene_template = os.path.join(args.output_scene_dir, scene_template)
-  blend_template = os.path.join(args.output_blend_dir, blend_template)
+  arr_template = '%s%%0%dd.npz' % (prefix, num_digits)
+  
+  img_template = osp.join(args.output_dir, 'train/images', img_template)
+  scene_template = osp.join(args.output_dir, 'train/scenes', scene_template)
+  blend_template = osp.join(args.output_dir, 'train/blend', blend_template)
+  arr_template = osp.join(args.output_dir, 'train/arr', arr_template)
 
-  if not os.path.isdir(args.output_image_dir):
-    os.makedirs(args.output_image_dir)
-  if not os.path.isdir(args.output_scene_dir):
-    os.makedirs(args.output_scene_dir)
-  if args.save_blendfiles == 1 and not os.path.isdir(args.output_blend_dir):
-    os.makedirs(args.output_blend_dir)
+  if not os.path.isdir(args.output_dir):
+    os.makedirs(args.output_dir)
+
+  if not os.path.isdir(osp.join(args.output_dir, 'train/images')):
+    os.makedirs(osp.join(args.output_dir, 'train/images'))
+
+  if not os.path.isdir(osp.join(args.output_dir, 'train/scenes')):
+    os.makedirs(osp.join(args.output_dir, 'train/scenes'))
+
+  if not os.path.isdir(osp.join(args.output_dir, 'train/arr')):
+    os.makedirs(osp.join(args.output_dir, 'train/arr'))
+
+  if args.save_blendfiles == 1 and not os.path.isdir(osp(args.output_dir, 'train/blend')):
+    os.makedirs(osp(args.output_dir, 'train/blend'))
   
   all_scene_paths = []
   for i in range(args.num_images):
+
+    print("GENERATING FRAME {0}/{1}".format(i, args.num_images))
+
     img_path = img_template % (i + args.start_idx)
     scene_path = scene_template % (i + args.start_idx)
+    arr_path = arr_template % (i + args.start_idx)
+
     all_scene_paths.append(scene_path)
     blend_path = None
     if args.save_blendfiles == 1:
       blend_path = blend_template % (i + args.start_idx)
     num_objects = random.randint(args.min_objects, args.max_objects)
+
     render_scene(args,
       num_objects=num_objects,
       output_index=(i + args.start_idx),
       output_split=args.split,
       output_image=img_path,
       output_scene=scene_path,
+      output_arr=arr_path,
       output_blendfile=blend_path,
+      scene_idx=i
     )
 
-  # After rendering all images, combine the JSON files for each scene into a
-  # single JSON file.
-  all_scenes = []
-  for scene_path in all_scene_paths:
-    with open(scene_path, 'r') as f:
-      all_scenes.append(json.load(f))
-  output = {
-    'info': {
-      'date': args.date,
-      'version': args.version,
-      'split': args.split,
-      'license': args.license,
-    },
-    'scenes': all_scenes
-  }
-  with open(args.output_scene_file, 'w') as f:
-    json.dump(output, f)
+  exit()
 
 
 
@@ -213,43 +213,58 @@ def render_scene(args,
     output_split='none',
     output_image='render.png',
     output_scene='render_json',
+    output_arr='arr',
     output_blendfile=None,
+    scene_idx=0
   ):
+  
+  for object_name in bpy.data.objects.keys():
+    if 'Sphere' in object_name or\
+       'Cylinder' in object_name or\
+       'Cube' in object_name or\
+       'Duck' in object_name or\
+       'Peg' in object_name or\
+       'Disk' in object_name or\
+       'Bowl' in object_name:
+       utils.delete_object_by_name(object_name)
 
   # Load the main blendfile
-  bpy.ops.wm.open_mainfile(filepath=args.base_scene_blendfile)
+  # bpy.ops.wm.open_mainfile(filepath=args.base_scene_blendfile)
 
-  # Load materials
-  utils.load_materials(args.material_dir)
-
-  # Set render arguments so we can get pixel coordinates later.
-  # We use functionality specific to the CYCLES renderer so BLENDER_RENDER
-  # cannot be used.
+  scene_struct = {}
   render_args = bpy.context.scene.render
-  render_args.engine = "CYCLES"
   render_args.filepath = output_image
-  render_args.resolution_x = args.width
-  render_args.resolution_y = args.height
-  render_args.resolution_percentage = 100
-  render_args.tile_x = args.render_tile_size
-  render_args.tile_y = args.render_tile_size
-  if args.use_gpu == 1:
-    # Blender changed the API for enabling CUDA at some point
-    if bpy.app.version < (2, 78, 0):
-      bpy.context.user_preferences.system.compute_device_type = 'CUDA'
-      bpy.context.user_preferences.system.compute_device = 'CUDA_0'
-    else:
-      cycles_prefs = bpy.context.user_preferences.addons['cycles'].preferences
-      cycles_prefs.compute_device_type = 'CUDA'
 
-  # Some CYCLES-specific stuff
-  bpy.data.worlds['World'].cycles.sample_as_light = True
-  bpy.context.scene.cycles.blur_glossy = 2.0
-  bpy.context.scene.cycles.samples = args.render_num_samples
-  bpy.context.scene.cycles.transparent_min_bounces = args.render_min_bounces
-  bpy.context.scene.cycles.transparent_max_bounces = args.render_max_bounces
-  if args.use_gpu == 1:
-    bpy.context.scene.cycles.device = 'GPU'
+  if scene_idx == 0:
+    # Load materials
+    utils.load_materials(args.material_dir)
+
+    # Set render arguments so we can get pixel coordinates later.
+    # We use functionality specific to the CYCLES renderer so BLENDER_RENDER
+    # cannot be used.
+    render_args.engine = "CYCLES"
+    render_args.resolution_x = args.width
+    render_args.resolution_y = args.height
+    render_args.resolution_percentage = 100
+    render_args.tile_x = args.render_tile_size
+    render_args.tile_y = args.render_tile_size
+    if args.use_gpu == 1:
+      # Blender changed the API for enabling CUDA at some point
+      if bpy.app.version < (2, 78, 0):
+        bpy.context.user_preferences.system.compute_device_type = 'CUDA'
+        bpy.context.user_preferences.system.compute_device = 'CUDA_0'
+      else:
+        cycles_prefs = bpy.context.user_preferences.addons['cycles'].preferences
+        cycles_prefs.compute_device_type = 'CUDA'
+
+    # Some CYCLES-specific stuff
+    bpy.data.worlds['World'].cycles.sample_as_light = True
+    bpy.context.scene.cycles.blur_glossy = 2.0
+    bpy.context.scene.cycles.samples = args.render_num_samples
+    bpy.context.scene.cycles.transparent_min_bounces = args.render_min_bounces
+    bpy.context.scene.cycles.transparent_max_bounces = args.render_max_bounces
+    if args.use_gpu == 1:
+      bpy.context.scene.cycles.device = 'GPU'
 
   # This will give ground-truth information about the scene and its objects
   scene_struct = {
